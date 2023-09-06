2ae833f9f39720fcad1ac8793755233a6230cbfd
Paulo Romano <paulo.romano@fema.edu.br>
2023-09-06 17:42:35 -0300

    Implementação até a aula 9.50

---
+++ src/test/java/com/algaworks/ecommerce/jpql/NamedQueryTest.java
@@ -0,0 +1,54 @@
+package com.algaworks.ecommerce.jpql;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Pedido;
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.TypedQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class NamedQueryTest extends EntityManagerTest {
+
+    @Test
+    public void executarConsultaArquivoXMLEspecificoProduto() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.todos", Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXMLEspecificoPedido() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createNamedQuery("Pedido.todos", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXML() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createNamedQuery("Pedido.listar", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsulta() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.listarPorCategoria", Produto.class);
+        typedQuery.setParameter("categoria", 2);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assertions.assertFalse(lista.isEmpty());
+    }
+}

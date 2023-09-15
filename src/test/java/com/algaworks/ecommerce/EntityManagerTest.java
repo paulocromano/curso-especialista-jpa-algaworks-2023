9878f24f5e4c9aa3e26ee3dfc4eca82baf34cde3
Paulo Romano <paulo-romano_133@hotmail.com>
2023-09-14 22:04:46 -0300

    Finalização do curso (contém alguns erros no projeto)

--- src/test/java/com/algaworks/ecommerce/EntityManagerTest.java
+++ src/test/java/com/algaworks/ecommerce/EntityManagerTest.java
@@ -1,29 +1,12 @@
 package com.algaworks.ecommerce;
 
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-public class EntityManagerTest {
-
-    protected static EntityManagerFactory entityManagerFactory;
+public class EntityManagerTest extends EntityManagerFactoryTest {
 
     protected EntityManager entityManager;
-
-    @BeforeAll
-    public static void setUpBeforeClass() {
-        entityManagerFactory = Persistence
-                .createEntityManagerFactory("Ecommerce-PU");
-    }
-
-    @AfterAll
-    public static void tearDownAfterClass() {
-        entityManagerFactory.close();
-    }
 
     @BeforeEach
     public void setUp() {
---
+++ src/test/java/com/algaworks/ecommerce/multitenant/MultiTenantTest.java
@@ -0,0 +1,41 @@
+package com.algaworks.ecommerce.multitenant;
+
+import com.algaworks.ecommerce.EntityManagerFactoryTest;
+import com.algaworks.ecommerce.hibernate.EcmCurrentTenantIdentifierResolver;
+import com.algaworks.ecommerce.model.Produto;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MultiTenantTest extends EntityManagerFactoryTest {
+
+    @Test
+    public void usarAbordagemPorMaquina() {
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("algaworks_ecommerce");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        Produto produto1 = entityManager1.find(Produto.class, 1);
+        Assertions.assertEquals("Kindle", produto1.getNome());
+        entityManager1.close();
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("loja_ecommerce");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Produto produto2 = entityManager2.find(Produto.class, 1);
+        Assertions.assertEquals("Kindle Paperwhite", produto2.getNome());
+        entityManager2.close();
+    }
+
+    @Test
+    public void usarAbordagemPorSchema() {
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("algaworks_ecommerce");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        Produto produto1 = entityManager1.find(Produto.class, 1);
+        Assertions.assertEquals("Kindle", produto1.getNome());
+        entityManager1.close();
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("loja_ecommerce");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Produto produto2 = entityManager2.find(Produto.class, 1);
+        Assertions.assertEquals("Kindle Paperwhite", produto2.getNome());
+        entityManager2.close();
+    }
+}
